8c2dc153f3087379f958f9f5c4b4e6c7d60925d2
Tannugupta04 <163404216+Tannugupta04@users.noreply.github.com>
2024-03-23 16:18:09 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId= 144553
+let accountEmail="guptatannu3@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let  accountState;
+
+//accountId =2// not allowed
+accountEmail="hc@hc.com"
+accountPassword="212121"
+accountCity="Delhi"
+//console.log(accountId);
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
+
+/*
+prefr not to use var because of the issue of 
+block scope and functional scope
+*/
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer version
+//alert("3+3") there is a different syntax to write alert in node
+console.log(3+3)
+
+let name="tannu"//string
+let age=20//int
+let isLoggedIn=false//boolean
+let state;
+
+//PRIMITIVE DATATYPES
+//number=>2 to the power of 53
+//biginit
+//string=>""
+//boolean=>true/false
+//null =>is a standalone value it is a representation of empty value
+//undefined=>when value is not defined
+//symbol=>when we talk about react to find the uniqueness
+
+//OBJECT
+console.log(typeof"tannu");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+

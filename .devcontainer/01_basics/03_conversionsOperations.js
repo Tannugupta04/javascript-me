fe624cce651f4e064a903063a44662a67fdc1b33
Tannugupta04 <163404216+Tannugupta04@users.noreply.github.com>
2024-03-24 10:38:03 +0000

    01_variables

---
+++ .devcontainer/01_basics/03_conversionsOperations.js
@@ -0,0 +1,37 @@
+//let score="33"
+//let score="33abc"
+//let score=null
+//let score=undefined
+//let score=true
+//let score =false
+let score="tannu"// cannot converted to number
+
+
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//"33"=>33
+//"3abc"=>NaN
+//true=>1;false=>0
+
+
+//conversion in boolean
+let isLoggedIn=1
+
+let booleanIaLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIaLoggedIn);
+//1=>true
+//0=>false
+//""=>false
+//"tannu"=>true
+
+//conversion to string
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+

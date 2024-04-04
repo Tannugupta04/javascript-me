3c3cfc72f861fab97fb09a37395a2349118f1b77
Tannugupta04 <163404216+Tannugupta04@users.noreply.github.com>
2024-04-04 04:49:38 +0000

    07dates

--- .devcontainer/01_basics/07_dates.js
+++ .devcontainer/01_basics/07_dates.js
@@ -18,3 +18,57 @@ console.log(myDate.toDateString());//Wed Apr 03 2024
 console.log(myDate.toLocaleDateString());//4/3/2024
 console.log(myDate.toLocaleString());//4/3/2024, 2:25:53 PM
 console.log(myDate.toTimeString());//14:25:53 GMT+0000 (Coordinated Universal Time)
+
+//1.type of date
+console.log(typeof myDate);//object
+
+//2.to declare specific date
+
+//let myCreatedDate=new Date(2023,0,23)
+//console.log(myCreatedDate.toDateString());//Mon Jan 23 2023//months starts from xzero in js
+
+//3.another method to declare date
+
+// let myCreatedDate=new Date(2023,0,23,5,3)
+// console.log(myCreatedDate.toLocaleString());//1/23/2023, 5:03:00 AM
+
+//4.to get dates in a format of yy/mm/dd
+
+// let myCreatedDate=new Date("2023-01-23")
+// console.log(myCreatedDate.toLocaleString());
+
+//5.another format mostly used in india
+let myCreatedDate=new Date("01-14-2023")
+console.log(myCreatedDate.toLocaleString());
+
+//6.time stamps:to find the exact time stamp for example quizez in js and poll desined in js
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp);//v 1712205193232 in mili second
+//7.to get the value
+console.log(myCreatedDate.getTime());
+//8. to convert in seconds
+console.log(Math.floor(Date.now()/1000));//INTERVIEW QUESTION TO CONVERT MILI SEC IN SECONDS
+//to avoid points/decimal we use math.floor
+//output in seconds =1712205447
+
+//9.
+let newDate=new Date()
+console.log(newDate);//2024-04-04T04:41:43.255Z
+console.log(newDate.getDate());//4
+
+console.log(newDate.getDay());//4
+console.log(newDate.getHours());//4
+console.log(newDate.getFullYear());//2024
+console.log(newDate.getMinutes());//41
+
+
+///mostly usd method
+
+newDate.toLocaleString('default',{
+    weekday:"long",
+    
+
+
+})
+//default k baad object likhte h

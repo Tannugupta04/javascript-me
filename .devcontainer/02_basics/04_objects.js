5a12ec01d8a2ef4abdd53b939bcaabdea4a77db4
Tannugupta04 <163404216+Tannugupta04@users.noreply.github.com>
2024-04-11 10:06:06 +0000

    04objects

--- .devcontainer/02_basics/04_objects.js
+++ .devcontainer/02_basics/04_objects.js
@@ -33,9 +33,43 @@ const obj1={1:"a",2:"b"}
 const obj2  ={3:"a",4:"b"}
 //const obj3={obj1,obj2}//{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
 
-const obj3=Object.assign({} ,obj1,obj2)//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+//const obj3=Object.assign({} ,obj1,obj2)//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+const obj3={...obj1,...obj2};//here using spread oerator
+//best way
 console.log(obj3);
+//object .assign k through we can assign objects
 
 
 
+const users=[
+    {
+    id:1,
+    email:"abc@gmail.com",
+},
+{
+    id:1,
+    email:"abc@gmail.com",
 
+},
+{
+    id:1,
+    email:"abc@gmail.com",
+},
+]
+
+//arrray me bhut sare objects hai usko loop thorugh krna ya value print krna aesse hi use krna hoga
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));//output value of datataype =array
+//here we have put all keys in an array  we can apply loop on that array and do many more operatins easuly
+console.log(Object.values(tinderUser));
+
+//enteries
+//each key value converted in an individual array
+console.log(Object.entries(tinderUser));
+
+//so,etime times when we loopthrough then if value does not eixts then
+//it makes chances of crash  so we ask it like
+///hasownproperty
+console.log(tinderUser.hasOwnProperty('isLogged'));

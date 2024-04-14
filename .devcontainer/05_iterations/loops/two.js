8cd1d30b50777e13cc4080fd4b41db21b50817a8
Tannugupta04 <163404216+Tannugupta04@users.noreply.github.com>
2024-04-14 11:36:43 +0000

    two

---
+++ .devcontainer/05_iterations/loops/two.js
@@ -0,0 +1,25 @@
+// while (condition) {
+    //isme bs condition hi checl hota
+//condition ka initialisation bhi hota
+// }
+let index=0
+while(index<=10){
+    //console.log(`value of index is ${index}`);
+    index=index+2
+}
+
+let myArray=['flash',"batman","superman"]
+let arr=0
+while(arr< myArray.length){
+   // console.log(`value is ${myArray[arr]}`);
+    arr=arr+1
+}
+
+
+///do while loop isme conditon baadd me hai
+let score=1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
+//kaam phle condiotn baaad me check is we do score=11 then puptu is score is11

66af11011af739f166fdc4924d9b6ddc81f859de
Tannugupta04 <163404216+Tannugupta04@users.noreply.github.com>
2024-04-14 05:40:29 +0000

    one

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,31 @@
+//hmesha pura code execute nhi hona chahiye kuch na kuch conditionals pe run hona chahie
+//for example agr hum login hai toh kuch code run ho agr logout ho toh alg
+//this is called control flow or logic flow
+
+//first control flow==If
+// if (condition){
+
+// }//ye conditon truw honi chahiye to hi internal code execute hoga
+//if condition==true then execute if false then  not execute
+
+
+//comparision krne pdenge to evaluate true or false
+
+// const isUserLoggedIn=true
+// if(isUserLoggedIn){
+
+// }
+//comaprison operator(<,>,<=,>=,!=,==,===,!==(check negqative))
+// if(31=2){
+//     //evaluation will be true
+// }
+
+//=== it also checks type//also known as strict equal to check datatyope if impt
+// if (2==="2"){
+//     console.log("executed");
+// }
+const temperature =41
+if(temperature<50){
+    console.log("less than 50");
+}
+console.log("temperature is greater than 50");

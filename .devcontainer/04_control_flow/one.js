9b9ed8a15068c5f6abec9eff97d9c49eca2d5bd0
Tannugupta04 <163404216+Tannugupta04@users.noreply.github.com>
2024-04-14 09:49:41 +0000

    one

--- .devcontainer/04_control_flow/one.js
+++ .devcontainer/04_control_flow/one.js
@@ -24,8 +24,72 @@
 // if (2==="2"){
 //     console.log("executed");
 // }
-const temperature =41
-if(temperature<50){
-    console.log("less than 50");
+// const temperature =41
+// if(temperature<50){
+//     console.log("less than 50");
+// }
+// else{
+
+
+// console.log("temperature is greater than 50");
+// }
+// console.log("execute");
+
+//scope relate
+//block scope
+
+// const score=200
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power:${power}`);
+// }
+// console.log(`user power:${power}`);//here we will get an error as power is not defiend 
+//out of scope
+
+
+//short hand notataion 
+//implicit scope
+
+//const balance=1000
+// if(balance>500) console.log("test");
+
+//yha curly braces nhi lgae ye hota hai implicit scope
+//implicit =maan lete hai yha scope hai
+//it execute only in one line to write another we use comma as well
+// but copmma not recommended
+
+
+//nesting
+// if (balance<500){
+//     console.log("less than");
+// }
+// else if(balance<750){
+//     console.log("equal to 750 ");
+
+// }
+// else if(balance<900)
+// {
+//     console.log("greater than 750");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+//real life implementation
+
+const userLoggedIn=true
+const debitCard=true
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+if(userLoggedIn &&debitCard&&2==2){
+    //when we want to check both so this && operator check both of them
+    //if we want to check another condition rthen add it after &&using this
+    console.log("Allow to buy course");
 }
-console.log("temperature is greater than 50");
+// the pipe(|| sign is used to check mu,tiple condition act as or operator
+
+if(loggedInFromEmail||loggedInFromGoogle)//ya toh toh login emails es krlo ya google se
+   { console.log("User logged in");
+}
+
+//we can check multiple conditions

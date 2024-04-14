06fc008bf854c87b499403d98b7ce06bf1d65b1a
Tannugupta04 <163404216+Tannugupta04@users.noreply.github.com>
2024-04-14 10:09:07 +0000

    switch

---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,69 @@
+//switch statements
+
+//when we have to ch4ck a value by multiple conditions we use switch statements
+
+//syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//const month =3
+//age 12 months k liye check krna hai toh if else use krne se tym taking hoga toh we use switch case
+//key is that value jo hme check rkni hai here key=month
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+    
+//     default:
+//         ///agr kuch match nhi huaq toh default vala run
+//         console.log("default case match");
+
+//         break;
+// }
+//by default ye hota hai jha key match uske baad ka sara code 
+//match ho jata hai except defaul agr break na ho toh
+// if break  hota toh next cases ko match nhi krta
+
+
+
+//AGR HMARE PASS "STRING" HOTA THEN WE DO IN THE SAME WAY LIKE WE DO IN IF 
+
+
+// const month ="march"
+// switch (month) {
+//     case "jan":
+//         console.log("january");
+//         break;
+//     case "feb":
+//         console.log("feb");
+//         break;
+//     case "march":
+//         console.log("march");
+//         break;
+//     case "april":
+//         console.log("april");
+//         break;
+    
+//     default:
+//         ///agr kuch match nhi huaq toh default vala run
+//         console.log("default case match");
+//         break;
+// }
+
+
